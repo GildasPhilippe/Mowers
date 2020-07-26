1baeecb58485b1bb137ea174cf50ddaa8ff119f2
GildasPhilippe <gildas.philippe56@gmail.com>
2020-07-26 19:01:36 +0200

    Testing all inputs when running main.py alone

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+from os import listdir
+from os.path import join
 import sys
 
 from mowers.simulator import Simulator
 
 
 def run_input(path):
+    print("TESTING {}".format(path))
     with open(path, "r") as f:
         input = f.read()
     simulator = Simulator(input)
     print(simulator.run())
+    print()
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         input_path = sys.argv[1]
+        run_input(input_path)
     else:
-        input_path = "inputs/input1.txt"
-    run_input(input_path)
+        input_folder = "inputs/"
+        inputs = [join(input_folder, fname) for fname in listdir(input_folder)]
+        for input_path in inputs:
+            run_input(input_path)
